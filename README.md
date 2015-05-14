8e7c203b1c212298046bab8ae4d3d9b2b248676c
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2015-05-14 16:39:55 -0400

    Huge improvement to storyboardxib support + drawing/re-drawing

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ In 2013, [**Matt D. Smith**](http://twitter.com/mds) unveiled the **[Float Label
 ![Sample Gif of Library in Action](http://d13yacurqjgara.cloudfront.net/users/6410/screenshots/1254439/form-animation-_gif_.gif)
 
 Image credit: [Matt D. Smith](http://twitter.com/mds). 
-### Changelog (v1.1.2)
-- Minor change to how clear button is accessed and modified 
-	- Thanks to [Tchelow](http://github.com/Tchelow)'s answer in [Issue #9](https://github.com/ArtSabintsev/UIFloatLabelTextField/issues/9#issuecomment-73449495)
+### Changelog (v1.2.0)
+- Fixed issues with floatLabel not properly appearing if text field was initialzied via xib or storyboard.
+- Improved how drawing rectangle is drawn during various states active, passive, editing, and non-editing states of text field.
 
 ### Features
 - Works with all view `init` styles
--- UIFloatLabelTextField/UIFloatLabelTextField.m
+++ UIFloatLabelTextField/UIFloatLabelTextField.m
@@ -47,6 +47,7 @@
     self = [super initWithCoder:aDecoder];
     if (self) {
         [self setup];
+        [self setPlaceholder:self.placeholder];
     }
     
     return self;
@@ -86,7 +87,7 @@
     
     // Enable clearButton when textField becomes firstResponder
     self.clearButtonMode = UITextFieldViewModeWhileEditing;
-
+    
     /*
      Observer for replicating `textField:shouldChangeCharactersInRange:replacementString:` UITextFieldDelegate method,
      without explicitly using UITextFieldDelegate.
@@ -204,10 +205,8 @@
 #pragma mark - Helpers
 - (UIEdgeInsets)floatLabelInsets
 {
-    return UIEdgeInsetsMake(_floatLabel.font.lineHeight,
-                            _horizontalPadding,
-                            0.0f,
-                            _horizontalPadding);
+    CGFloat top = [self.text length] ? 5.0f : 0.0f;
+    return UIEdgeInsetsMake(top, _horizontalPadding, 0.0f, _horizontalPadding);
 }
 
 - (void)textDidChange:(NSNotification *)notification
@@ -280,7 +279,6 @@
 {
     [super setPlaceholder:placeholder];
     
-    
     if ([placeholder length]) {
         _floatLabel.text = placeholder;
     }
@@ -333,7 +331,7 @@
     if (![self isFirstResponder] && ![self.text length]) {
         [self toggleFloatLabelProperties:UIFloatLabelAnimationTypeHide];
     } else if ([self.text length]) {
-       [self toggleFloatLabelProperties:UIFloatLabelAnimationTypeShow];
+        [self toggleFloatLabelProperties:UIFloatLabelAnimationTypeShow];
     }
 }
 
@@ -346,7 +344,7 @@
      verticalPadding must be manually set if textField was initialized
      using NSAutoLayout constraints
      */
- 
+    
     _floatLabel.textColor = _floatLabelActiveColor;
     _storedText = [self text];
     
