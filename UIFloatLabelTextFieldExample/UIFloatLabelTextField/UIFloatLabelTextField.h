ce24d7394162d1853e287c355528a8d526881215
Arthur Ariel Sabintsev <arthur@sabintsev.com>
2017-04-20 20:24:11 -0400

    Added shared framework and rebuilt project

---
+++ UIFloatLabelTextFieldExample/UIFloatLabelTextField/UIFloatLabelTextField.h
@@ -0,0 +1,19 @@
+//
+//  UIFloatLabelTextField.h
+//  UIFloatLabelTextField
+//
+//  Created by Arthur Sabintsev on 4/20/17.
+//  Copyright © 2017 Arthur Ariel Sabintsev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIFloatLabelTextField.
+FOUNDATION_EXPORT double UIFloatLabelTextFieldVersionNumber;
+
+//! Project version string for UIFloatLabelTextField.
+FOUNDATION_EXPORT const unsigned char UIFloatLabelTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIFloatLabelTextField/PublicHeader.h>
+
+
